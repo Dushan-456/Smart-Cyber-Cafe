182309a816f4bb3f35ea0696b6ceed45d89c5bbe
Navodya Dushan <168759740+Dushan-456@users.noreply.github.com>
2025-05-30 22:28:27 +0530

    Update index.php

--- tables/index.php
+++ tables/index.php
@@ -20,36 +20,19 @@ if (session_status() === PHP_SESSION_NONE) {
 $table_number = $_SESSION["table_number"];
 
 
-// -------------Getting user Details-------------------
+if(isset($_POST['submit'])){
+  $_SESSION['customer_name'] = $_POST['customer_name'];
+  $customer_mobile_number = $_POST['customer_mobile_number'];
+  $customer_name = $_SESSION['customer_name'];
 
-// global $conn;
-// $select_user_query = "SELECT * FROM `employees` WHERE NIC_num	 = '$session_NIC';";
-// $result = mysqli_query($conn, $select_user_query);
-// $result_row = mysqli_fetch_assoc($result);
+}elseif(isset($_POST['skip'])){
+  $_SESSION['customer_name'] = "User";
 
-// $user_id = $result_row["id"];
-// $employee_id = $result_row["employee_id"];
-// $first_name = $result_row["first_name"];
-// $last_name = $result_row["last_name"];
-// $title = $result_row["title"];
-// $NIC_num = $result_row["NIC_num"];
-// $designation = $result_row["designation"];
-// $department = $result_row["department"];
-// $EPF_ETF_num = $result_row["EPF_ETF_num"];
-// $employee_type = $result_row["employee_type"];
-// $marital_status = $result_row["marital_status"];
-// $nationality = $result_row["nationality"];
-// $gender = $result_row["gender"];
-// $address = $result_row["address"];
-// $date_of_appointment	= $result_row["date_of_appointment"];
-// $birth_day = $result_row["birth_day"];
-// $mobile_no_personal = $result_row["mobile_no_personal"];
-// $mobile_no_official = $result_row["mobile_no_official"];
-// $email_personal = $result_row["email_personal"];
-// $email_official = $result_row["email_official"];
-// $profile_pic = $result_row["profile_pic"];
-// $emp_role = $result_row["emp_role"];
-// $theme = $result_row["theme"];
+}
+// $_SESSION['customer_name']= 'John Doe';
+
+// // $nameSet = "ggg";
+
 
 
 ?>
@@ -68,7 +51,10 @@ $table_number = $_SESSION["table_number"];
 <body>
 
 <div class="body-container">
+<p style="text-align: center;" ><?php if(isset($_SESSION['customer_name'])){ echo "Welcome ",$_SESSION["customer_name"];} ; ?></p>
+
   <div class="top">
+
     <div class="header ">
         <h2>Our Menu</h2>
         <h2> Table<?php echo " $table_number";  ?></h2>
@@ -95,28 +81,30 @@ if(isset( $_GET["category"])){
 <!-- // login popup -->
  
 
-<div id="loginModal" class="modal">
+<div id="loginModal" class="modal" style="display: <?php  if(isset($_SESSION['customer_name'])){ echo"none";}else{echo"block";} ; ?>;">
   <div class="modal-content">
    
         <div class="form-section">
-      <form action="">
-            <span class="close" onclick="closeModal()"><i class="fa-solid fa-xmark"></i></span>
+      <form action="" method="post">
+            <!-- <span class="close" onclick="closeModal()"><i class="fa-solid fa-xmark"></i></span> -->
 
         <h3>Your Details</h3>
         
-        <input type="text" required placeholder="Your Name">
-        <input type="text" required placeholder="Mobile Number">
+        <input type="text" name="customer_name" required placeholder="Your Name">
+        <input type="text" name="customer_mobile_number" required placeholder="Mobile Number">
         
         <div class="checkbox-container">
-          <input type="checkbox" id="remember" name="remember" />
-          <label for="remember">Allow promotion sms</label>
+          <input type="checkbox" id="remember" name="sms" />
+          <label for="sms">Allow promotion sms</label>
         </div>
         
-        <button class="continue" onclick="login()">Continue</button>
+        <button class="continue" name="submit" type="submit" >Continue</button>
+      </form>
+      <form action="" method="post">
+      <div class="or-divider">or</div>
+      <button class="facebook-btn" type="submit" name="skip" >Skip</button>
       </form>
 
-      <div class="or-divider">or</div>
-      <button class="facebook-btn" onclick="closeModal()">Skip</button>
     </div>
   </div>
 </div>

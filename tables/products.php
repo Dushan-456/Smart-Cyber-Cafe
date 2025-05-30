943f5e9c80c51b1dd654ed3b8ac013343f09b406
Navodya Dushan <168759740+Dushan-456@users.noreply.github.com>
2025-05-30 17:37:28 +0530

    Create products.php

---
+++ tables/products.php
@@ -0,0 +1,100 @@
+
+      <div class="tabs">
+        <div class="tab active">Fried Rice</div>
+ 
+      </div>
+</div>
+
+  <!-- Meals Grid -->
+  <div class="foodmenu active" id="meals">
+     <div class="food-card-menu">
+      <img src="https://www.theflavorbender.com/wp-content/uploads/2018/03/Chicken-Kottu-Roti-The-Flavor-Bender-Featured-Image-SQ-8.jpg" alt="Spicy Noodles">
+    <div>
+      <h4>Spicy Noodles <br>
+      Full</h4>
+      <p>Rs 500.00</p>
+    </div>
+    <div class="buy"><i class="fa-solid fa-cart-plus"></i></div>
+  </div>
+     <div class="food-card-menu">
+      <img src="https://www.theflavorbender.com/wp-content/uploads/2018/03/Chicken-Kottu-Roti-The-Flavor-Bender-Featured-Image-SQ-8.jpg" alt="Spicy Noodles">
+    <div>
+      <h4>Spicy Noodles <br>
+      Full</h4>
+      <p>Rs 500.00</p>
+    </div>
+    <div class="buy"><i class="fa-solid fa-cart-plus"></i></div>
+  </div>
+     <div class="food-card-menu">
+      <img src="https://www.theflavorbender.com/wp-content/uploads/2018/03/Chicken-Kottu-Roti-The-Flavor-Bender-Featured-Image-SQ-8.jpg" alt="Spicy Noodles">
+    <div>
+      <h4>Spicy Noodles <br>
+      Full</h4>
+      <p>Rs 500.00</p>
+    </div>
+    <div class="buy"><i class="fa-solid fa-cart-plus"></i></div>
+  </div>
+     <div class="food-card-menu">
+      <img src="https://www.theflavorbender.com/wp-content/uploads/2018/03/Chicken-Kottu-Roti-The-Flavor-Bender-Featured-Image-SQ-8.jpg" alt="Spicy Noodles">
+    <div>
+      <h4>Spicy Noodles <br>
+      Full</h4>
+      <p>Rs 500.00</p>
+    </div>
+    <div class="buy"><i class="fa-solid fa-cart-plus"></i></div>
+  </div>
+     <div class="food-card-menu">
+      <img src="https://www.theflavorbender.com/wp-content/uploads/2018/03/Chicken-Kottu-Roti-The-Flavor-Bender-Featured-Image-SQ-8.jpg" alt="Spicy Noodles">
+    <div>
+      <h4>Spicy Noodles <br>
+      Full</h4>
+      <p>Rs 500.00</p>
+    </div>
+    <div class="buy"><i class="fa-solid fa-cart-plus"></i></div>
+  </div>
+     <div class="food-card-menu">
+      <img src="https://www.theflavorbender.com/wp-content/uploads/2018/03/Chicken-Kottu-Roti-The-Flavor-Bender-Featured-Image-SQ-8.jpg" alt="Spicy Noodles">
+    <div>
+      <h4>Spicy Noodles <br>
+      Full</h4>
+      <p>Rs 500.00</p>
+    </div>
+    <div class="buy"><i class="fa-solid fa-cart-plus"></i></div>
+  </div>
+     <div class="food-card-menu">
+      <img src="https://www.theflavorbender.com/wp-content/uploads/2018/03/Chicken-Kottu-Roti-The-Flavor-Bender-Featured-Image-SQ-8.jpg" alt="Spicy Noodles">
+    <div>
+      <h4>Spicy Noodles <br>
+      Full</h4>
+      <p>Rs 500.00</p>
+    </div>
+    <div class="buy"><i class="fa-solid fa-cart-plus"></i></div>
+  </div>
+     <div class="food-card-menu">
+      <img src="https://www.theflavorbender.com/wp-content/uploads/2018/03/Chicken-Kottu-Roti-The-Flavor-Bender-Featured-Image-SQ-8.jpg" alt="Spicy Noodles">
+    <div>
+      <h4>Spicy Noodles <br>
+      Full</h4>
+      <p>Rs 500.00</p>
+    </div>
+    <div class="buy"><i class="fa-solid fa-cart-plus"></i></div>
+  </div>
+     <div class="food-card-menu">
+      <img src="https://www.theflavorbender.com/wp-content/uploads/2018/03/Chicken-Kottu-Roti-The-Flavor-Bender-Featured-Image-SQ-8.jpg" alt="Spicy Noodles">
+    <div>
+      <h4>Spicy Noodles <br>
+      Full</h4>
+      <p>Rs 500.00</p>
+    </div>
+    <div class="buy"><i class="fa-solid fa-cart-plus"></i></div>
+  </div>
+    <div class="food-card-menu">
+      <img src="https://www.theflavorbender.com/wp-content/uploads/2018/03/Chicken-Kottu-Roti-The-Flavor-Bender-Featured-Image-SQ-8.jpg" alt="Spicy Noodles">
+    <div>
+      <h4>Spicy Noodles <br>
+      Full</h4>
+      <p>Rs 500.00</p>
+    </div>
+    <div class="buy"><i class="fa-solid fa-cart-plus"></i></div>
+  </div>
+  </div>

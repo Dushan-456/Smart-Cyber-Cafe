3fceb14ce882dde409b202366c37206f06b62051
Navodya Dushan <168759740+Dushan-456@users.noreply.github.com>
2025-05-30 17:04:56 +0530

    Update db.php

--- config/db.php
+++ config/db.php
@@ -1,11 +1,16 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    
-</body>
-</html>
+<?php
+$servername = "localhost:3306";
+$username = "root";
+$password = "";
+$database = "cybercafe_sys";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $database);
+
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+// echo "Connected successfully";
+
+

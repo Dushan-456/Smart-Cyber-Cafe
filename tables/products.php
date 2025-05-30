d08a3e9328d2abcc9b387955eb7c235730fddf52
Navodya Dushan <168759740+Dushan-456@users.noreply.github.com>
2025-05-30 22:28:31 +0530

    Update products.php

--- tables/products.php
+++ tables/products.php
@@ -1,102 +1,71 @@
 
-      <div class="tabs">
+     <?php 
+       $category = $_GET["category"];
+       $category_name_query = "SELECT * FROM `categories`   WHERE  id = '$category'";
+       $food_category_name_result = mysqli_query($conn,$category_name_query);
+       $category_name_row = mysqli_fetch_assoc($food_category_name_result);
+       $category_name = $category_name_row["name"];
+
+     
+     ?>
+     
+     
+     
+     
+     <div class="tabs">
         <div ><a class="back-btn" href="?menu"><i class="fa-solid fa-arrow-left"></i> &nbsp; Back</a></div>
-        <div class="tab active">Fried Rice</div>
+        <div class="tab active"><?php echo $category_name ?></div>
         <div class="tab "></div>
  
       </div>
 </div>
 
   <!-- Meals Grid -->
-  <div class="foodmenu active" id="meals">
-     <div class="food-card-menu">
-      <img src="https://www.theflavorbender.com/wp-content/uploads/2018/03/Chicken-Kottu-Roti-The-Flavor-Bender-Featured-Image-SQ-8.jpg" alt="Spicy Noodles">
-    <div>
-      <h4>Spicy Noodles <br>
-      Full</h4>
-      <p>Rs 500.00</p>
-    </div>
-    <div class="buy"><i class="fa-solid fa-cart-plus"></i></div>
-  </div>
-     <div class="food-card-menu">
-      <img src="https://www.theflavorbender.com/wp-content/uploads/2018/03/Chicken-Kottu-Roti-The-Flavor-Bender-Featured-Image-SQ-8.jpg" alt="Spicy Noodles">
-    <div>
-      <h4>Spicy Noodles <br>
-      Full</h4>
-      <p>Rs 500.00</p>
-    </div>
-    <div class="buy"><i class="fa-solid fa-cart-plus"></i></div>
-  </div>
-     <div class="food-card-menu">
-      <img src="https://www.theflavorbender.com/wp-content/uploads/2018/03/Chicken-Kottu-Roti-The-Flavor-Bender-Featured-Image-SQ-8.jpg" alt="Spicy Noodles">
-    <div>
-      <h4>Spicy Noodles <br>
-      Full</h4>
-      <p>Rs 500.00</p>
-    </div>
-    <div class="buy"><i class="fa-solid fa-cart-plus"></i></div>
-  </div>
-     <div class="food-card-menu">
-      <img src="https://www.theflavorbender.com/wp-content/uploads/2018/03/Chicken-Kottu-Roti-The-Flavor-Bender-Featured-Image-SQ-8.jpg" alt="Spicy Noodles">
-    <div>
-      <h4>Spicy Noodles <br>
-      Full</h4>
-      <p>Rs 500.00</p>
-    </div>
-    <div class="buy"><i class="fa-solid fa-cart-plus"></i></div>
-  </div>
-     <div class="food-card-menu">
-      <img src="https://www.theflavorbender.com/wp-content/uploads/2018/03/Chicken-Kottu-Roti-The-Flavor-Bender-Featured-Image-SQ-8.jpg" alt="Spicy Noodles">
-    <div>
-      <h4>Spicy Noodles <br>
-      Full</h4>
-      <p>Rs 500.00</p>
-    </div>
-    <div class="buy"><i class="fa-solid fa-cart-plus"></i></div>
-  </div>
-     <div class="food-card-menu">
-      <img src="https://www.theflavorbender.com/wp-content/uploads/2018/03/Chicken-Kottu-Roti-The-Flavor-Bender-Featured-Image-SQ-8.jpg" alt="Spicy Noodles">
-    <div>
-      <h4>Spicy Noodles <br>
-      Full</h4>
-      <p>Rs 500.00</p>
-    </div>
-    <div class="buy"><i class="fa-solid fa-cart-plus"></i></div>
-  </div>
-     <div class="food-card-menu">
-      <img src="https://www.theflavorbender.com/wp-content/uploads/2018/03/Chicken-Kottu-Roti-The-Flavor-Bender-Featured-Image-SQ-8.jpg" alt="Spicy Noodles">
-    <div>
-      <h4>Spicy Noodles <br>
-      Full</h4>
-      <p>Rs 500.00</p>
-    </div>
-    <div class="buy"><i class="fa-solid fa-cart-plus"></i></div>
-  </div>
-     <div class="food-card-menu">
-      <img src="https://www.theflavorbender.com/wp-content/uploads/2018/03/Chicken-Kottu-Roti-The-Flavor-Bender-Featured-Image-SQ-8.jpg" alt="Spicy Noodles">
-    <div>
-      <h4>Spicy Noodles <br>
-      Full</h4>
-      <p>Rs 500.00</p>
-    </div>
-    <div class="buy"><i class="fa-solid fa-cart-plus"></i></div>
-  </div>
-     <div class="food-card-menu">
-      <img src="https://www.theflavorbender.com/wp-content/uploads/2018/03/Chicken-Kottu-Roti-The-Flavor-Bender-Featured-Image-SQ-8.jpg" alt="Spicy Noodles">
-    <div>
-      <h4>Spicy Noodles <br>
-      Full</h4>
-      <p>Rs 500.00</p>
-    </div>
-    <div class="buy"><i class="fa-solid fa-cart-plus"></i></div>
-  </div>
-    <div class="food-card-menu">
-      <img src="https://www.theflavorbender.com/wp-content/uploads/2018/03/Chicken-Kottu-Roti-The-Flavor-Bender-Featured-Image-SQ-8.jpg" alt="Spicy Noodles">
-    <div>
-      <h4>Spicy Noodles <br>
-      Full</h4>
-      <p>Rs 500.00</p>
-    </div>
-    <div class="buy"><i class="fa-solid fa-cart-plus"></i></div>
-  </div>
+  <div class="foodmenu active" id="">
+
+  <?php
+    
+    global $conn;
+
+    $foods_query = "SELECT * FROM `products` WHERE category_id = '$category' ORDER BY `name` ASC";
+    $foods_result = mysqli_query($conn,$foods_query);
+    if(mysqli_num_rows($foods_result)>0){
+    while($foods_result_row = mysqli_fetch_assoc($foods_result)){
+
+            $product_id = $foods_result_row["id"];
+            $Product_name = $foods_result_row["name"];
+            $product_image = $foods_result_row["image"];
+            $product_price = $foods_result_row["price"];
+
+            echo"
+             <div class='food-card-menu'>
+                  <img src='../assest/images/products/$product_image' alt='$Product_name'>
+                        <div>
+                          <h4>$Product_name</h4>
+                          <p>Rs $product_price</p>
+                        </div>
+                <div class='buy'><i class='fa-solid fa-cart-plus'></i></div>
+            </div>
+            
+            ";
+
+                 }
+                }else{
+                  echo"
+                     <div class='food-card-menu'>
+                        <div>
+                          <h4>Sorry No any Products in $category_name Category </h4>
+                        </div>
+                <div class='buy'></div>
+            </div>
+                  ";
+                }
+
+ 
+
+  
+  ?>
+
+    
+   
   </div>
